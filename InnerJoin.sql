a64f5e6ef70178df6473606477ea83b86aa7ee22
Liudmyla Sovetovs <milkaegik@gmail.com>
2024-11-07 22:59:54 -0800

    tidy up

--- InnerJoin.sql
+++ InnerJoin.sql
@@ -1,22 +1,19 @@
-SELECT categories.id,
-categories.name as category_name, categories.description as category_description, customers.id as customer_id,
-customers.name as customer_name, customers.contact as customer_contact, customers.address as customer_address,
-customers.city as customer_city, customers.postal_code as customer_postal_code, customers.country as customer_country,
-employees.employee_id, employees.last_name as employee_last_name, employees.first_name as employee_first_name,
-employees.birthdate as employee_birthdate, employees.photo as employee_photo, employees.notes as employee_notes,
-order_details.id as order_detail_id, order_details.quantity as order_detail_quantity, 
-orders.id as order_id, orders.date as order_date,
-products.id as product_id, products.name as product_name, products.unit as product_unit, products.price as product_price,
-shippers.id as shipper_id, shippers.name as shipper_name, shippers.phone as shipper_phone,
-suppliers.id as supplier_id, suppliers.name as supplier_name, suppliers.contact as supplier_contact,
-suppliers.address as supplier_address, suppliers.city as supplier_city, suppliers.postal_code as supplier_postal_code,
-suppliers.country as supplier_country, suppliers.phone as supplier_phone
+SELECT COUNT(categories.name) as category_name, AVG(order_details.quantity) as quantity_ord
 FROM orders
 INNER JOIN order_details ON orders.id = order_details.order_id
 INNER JOIN customers ON orders.customer_id = customers.id
 INNER JOIN employees ON orders.employee_id = employees.employee_id
 INNER JOIN shippers ON orders.shipper_id = shippers.id
-INNER JOIN products ON products.id = order_details.product_id
-INNER JOIN suppliers ON suppliers.id = products.supplier_id
-INNER JOIN categories ON categories.id = products.category_id;
+INNER JOIN products ON order_details.product_id = products.id
+INNER JOIN suppliers ON products.supplier_id = suppliers.id
+INNER JOIN categories ON products.category_id = categories.id
+WHERE employees.employee_id > 3 AND employees.employee_id <= 10
+GROUP BY categories.name
+HAVING quantity_ord > 21
+ORDER BY category_name DESC
+LIMIT 4
+OFFSET 1;
+
+
+
 
